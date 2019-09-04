8b58135bda2dd502ee7a065101e05709e8388ee4
dmitromart@gmail.com <dmitromart@gmail.com>
2019-09-04 22:53:15 +0300

    Update Credit Page

--- Tresu/Tresu/CreditCardPage.xaml.cs
+++ Tresu/Tresu/CreditCardPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,56 @@ namespace Tresu
         public CreditCardPage()
         {
             InitializeComponent();
+            creditChoose.SelectedIndex = 0;
+        }
+
+        private void CreditChoose_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (creditChoose.SelectedIndex == 0)
+            {
+                donate.Visibility = Visibility.Hidden;
+                LabelDonate.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                donate.Visibility = Visibility.Visible;
+                LabelDonate.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string regex = "\\d{4}[\\s,\\S]\\d{4}[\\s,\\S]\\d{4}[\\s,\\S]\\d{4}";
+            var match = Regex.Match(nomerCard.Text, regex, RegexOptions.IgnoreCase);
+
+            if (creditChoose.SelectedIndex == 0)
+            {
+
+                    if (creditChoose.Text != "" && CVVCbox.Text.Length == 3 && month.Text != "" && year.Text != "")
+                    {
+                        if (match.Success)
+                        {
+                            MessageBox.Show("OK");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please fill all the fields");
+                    }
+                
+            }
+            else if (creditChoose.Text != "" && CVVCbox.Text.Length == 3 && month.Text != "" && year.Text != "" && donate.Text != "")
+            {
+                if (match.Success)
+                {
+                   MessageBox.Show("OK");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill all the fields(Donate)");
+            }
+            
         }
     }
 }
